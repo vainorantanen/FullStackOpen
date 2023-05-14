b671fa55add2a0e31d2a4736b861fc28033d808a
Väinö Rantanen <vaino01@hotmail.fi>
2023-05-14 19:41:00 +0300

    tehty 9.15 asti

--- osa9/my-first-app-part-d/src/App.tsx
+++ osa9/my-first-app-part-d/src/App.tsx
@@ -16,23 +16,91 @@ const App = () => {
     parts: CoursePart[];
   }
 
-  interface CoursePart {
+  interface CoursePartBase {
     name: string;
     exerciseCount: number;
   }
 
+  interface CoursePartDesc extends CoursePartBase {
+    description: string,
+  }
+  
+  interface CoursePartBasic extends CoursePartDesc {
+    kind: "basic"
+  }
+  
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+  
+  interface CoursePartBackground extends CoursePartDesc {
+    backgroundMaterial: string;
+    kind: "background"
+  }
+
+  interface CoursePartSpecial extends CoursePartDesc {
+    requirements: string[];
+    kind: "special"
+  }
+
+  type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
+
   const Content = (props: ContentProps) => {
-    console.log(props)
+  console.log("TÄSÄ", props.parts)
     return (
     <div>
       {props.parts.map((p, index) => (
-        <p key={index}>
-          {p.name} {p.exerciseCount}
-        </p>
+        <div key={index}>
+          <Part {...p} />
+        </div>
       ))}
-  </div>
-    );
-  };
+    </div>
+  );
+};
+
+const Part = (p: CoursePart) => {
+  console.log("PEE", p);
+  switch (p.kind) {
+    case "basic":
+      return (
+        <div>
+          <p style={{ fontWeight: 'bold'}}>{p.name} {p.exerciseCount}</p>
+          <p style={{ fontStyle: 'italic' }}>Description: {p.description}</p>
+        </div>
+      );
+    case "group":
+      return (
+        <div>
+          <p style={{ fontWeight: 'bold'}}>{p.name} {p.exerciseCount}</p>
+          <p>Group projects: {p.groupProjectCount}</p>
+        </div>
+      );
+    case "background":
+      return (
+        <div>
+          <p style={{ fontWeight: 'bold'}}>{p.name} {p.exerciseCount}</p>
+          <p style={{ fontStyle: 'italic' }}>Description: {p.description}</p>
+          <p>Background material: {p.backgroundMaterial}</p>
+        </div>
+      );
+    case "special":
+      return (
+        <div>
+           <p style={{ fontWeight: 'bold'}}>{p.name} {p.exerciseCount}</p>
+          <p style={{ fontStyle: 'italic' }}>Description: {p.description}</p>
+          Requirements: {p.requirements.map((r, index) => (
+            <div key={index}>
+              <p>- {r}</p>
+            </div>
+          ))}
+        </div>
+      );
+    default:
+      return null;
+  }
+};
 
 const Total = (props: ContentProps) => {
   return (
@@ -44,18 +112,44 @@ const Total = (props: ContentProps) => {
 }
 
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
